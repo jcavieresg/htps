c9138181be6db49d84abf926868a80a2f45f1110
Joaquin Cavieres <55828236+jcavieresg@users.noreply.github.com>
2023-10-12 06:31:11 -0300

    Update build_package.R

--- r_codes/build_package.R
+++ r_codes/build_package.R
@@ -1,20 +1,12 @@
 rm(list = ls())
-setwd("C:/Users/Usuario/Desktop/mypackage")
-
-#setwd("C:/Users/Usuario/Desktop/example2")
+setwd("C:/Users/Usuario/Desktop/htps")
 
 library(devtools)
 library(Rcpp)
 library(RcppArmadillo)
 library(pkgKitten)
 
-#RcppArmadillo.package.skeleton( "mypackage" )
-
 compileAttributes(verbose=TRUE) # Find and register Rcpp functions 
-devtools::load_all() # load all functions
-
-# #pkgbuild::compile_dll()
-# devtools::document() # create roxygen2 document, don't make .md by hand, let roxygen2 do it
-# devtools::check() # check whether the package is OK
 # devtools::build() # build a package
+devtools::load_all() # load all functions
 
